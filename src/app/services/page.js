a6a8098b8f79efbcecd6fbdc21219bddcd3cbc9f
nuclei-7 <govindKumar77727@gmail.com>
2024-04-07 15:44:47 +0530

    Signed-off-by: nuclei-7 <govindKumar77727@gmail.com>
    final

--- src/app/services/page.js
+++ src/app/services/page.js
@@ -1,27 +1,26 @@
 import React from 'react';
-import styles from '@/app/styles/services.module.css'
+import styles from '@/app/styles/services.module.css';
 
-
-const services = ()=>{
-    return(<>
-
-    
-        <h3>This is services p <div className={styles.services}>
-      <h1>Our Services</h1>
-      <div className={styles.service}>
-        <h2>Service 1</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam aliquet tortor eu urna dapibus, sit amet fringilla lacus gravida. Duis ut turpis id risus suscipit sodales.</p>
+const Services = () => {
+  return (
+    <>
+      <div className={styles.services}>
+        <h1>Our Services</h1>
+        <div className={styles.service}>
+          <h2>Spurious Hit</h2>
+          <p>An Educational Tech blogging Website: <a href="http://www.spurioushit.com">Click Here</a></p>
+        </div>
+        <div className={styles.service}>
+          <h2>Aurum Gold pay</h2>
+          <p>A Payment Application with Gold: <a href="https://gold-pay-web-urg8.vercel.app/">Click Here</a></p>
+        </div>
+        <div className={styles.service}>
+          <h2>Yo Pal !</h2>
+          <p>A Social Media Application: <a href="http://yo-pal.in">Click Here</a></p>
+        </div>
       </div>
-      <div className={styles.service}>
-        <h2>Service 2</h2>
-        <p>Curabitur nec justo nec quam placerat pulvinar ut vel lectus. Vestibulum auctor felis vitae velit gravida feugiat. Vivamus id libero eget sapien tincidunt volutpat nec nec nulla.</p>
-      </div>
-      <div className={styles.service}>
-        <h2>Service 3</h2>
-        <p>Integer efficitur quam nec gravida condimentum. Aenean auctor urna in lectus pulvinar ultrices. Sed nec aliquam odio. Morbi auctor mi sed urna consectetur, id congue velit ultricies.</p>
-      </div>
-    </div>age </h3>
+    </>
+  );
+};
 
-    </>)
-}
-export default services;
+export default Services;
